258ee9f8b7c38e388b1b28d8f0bb31b3078731d9
Naim AK <149870242+naimak659@users.noreply.github.com>
2024-12-15 14:07:28 +0600

    product image html done✅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primaryFont: "#364a63",
+        secondaryFont: "#8091a7",
+        purpleFont: "#6576ff",
+        purple_watch: "#816bff",
+        cyan_watch: "#1fcec9",
+        blue_watch: "#4b97d3",
+        dark_watch: "#3b4747",
+      },
+    },
   },
   plugins: [],
 };
