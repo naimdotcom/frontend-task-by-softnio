6c1d9e277db5df738b7e6f8a2d5111ddaccd4dc4
Naim AK <149870242+naimak659@users.noreply.github.com>
2024-12-15 21:21:25 +0600

    modal popup done and the checkout done.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const decrementBtn = document.getElementById("decrement");
 const incrementBtn = document.getElementById("increment");
 const countOfProduct = document.getElementById("count");
 const addToCardBtn = document.getElementById("add-to-card");
+const modal = document.getElementById("modal");
+const modalCart = document.getElementById("modal-cart");
+const checkoutBtn = document.getElementById("open-check-out");
+const closeBtn = document.getElementById("close-check-out");
+const finalCheckOutBtn = document.getElementById("final-check-out");
 
 const checkoutProducts = [];
 let addToCardProduct = {
@@ -69,3 +74,111 @@ addToCardBtn.addEventListener("click", () => {
   checkoutProducts.push(addToCardProduct);
   console.log(checkoutProducts);
 });
+
+// todo: checkout close and open
+checkoutBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  checkOutProductModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// todo: final check out
+function checkOutProductModal() {
+  const modalCartBody = document.getElementById("modal-cart-body");
+  const modalCartTotal = document.getElementById("modal-cart-total");
+  let checkoutproductsHtml = "";
+  let checkoutTotalHtml = "";
+  let totalProductCount = 0;
+  let totalProductPrice = 0;
+
+  if (checkoutProducts.length === 0) {
+    modalCartTotal.innerHTML = `No products added to cart`;
+    return;
+  }
+
+  checkoutProducts.forEach((product) => {
+    // todo: add the product amount and count
+    totalProductCount += parseInt(product.productCount);
+    totalProductPrice += parseInt(product.productPrice * product.productCount);
+
+    // todo: add the product in html
+    checkoutproductsHtml += `
+      <tr class="border-b">
+                    <td class="py-4">
+                      <div class="flex items-center pr-14">
+                        <img
+                          class="w-12 h-12 mr-4 rounded-md"
+                          src="${product.productImage}"
+                          alt="${product.productName}"
+                        />
+                        <h2
+                          class="text-sm font-normal leading-normal text-primaryFont font-roboto"
+                        >
+                          ${product.productName}
+                        </h2>
+                      </div>
+                    </td>
+                    <td class="">
+                      <h3
+                        class="text-sm font-normal leading-normal text-center text-primaryFont font-roboto"
+                      >
+                        ${product.productColor}
+                      </h3>
+                    </td>
+                    <td class="">
+                      <h3
+                        class="text-sm font-normal leading-normal text-center text-primaryFont font-roboto"
+                      >
+                        ${product.productSize}
+                      </h3>
+                    </td>
+                    <td class="px-4 py-4">
+                      <h3
+                        class="text-sm font-bold leading-normal text-center text-primaryFont font-roboto"
+                      >
+                        ${product.productCount}
+                      </h3>
+                    </td>
+                    <td class="py-4 font-bold text-gray-800">
+                      <h3
+                        class="text-sm font-bold leading-normal text-right text-primaryFont font-roboto"
+                      >
+                        $${product.productPrice * product.productCount}
+                      </h3>
+                    </td>
+                  </tr>
+                  `;
+
+    checkoutTotalHtml = `
+      <tr>
+                    <td
+                      colspan="3"
+                      class="py-4 font-bold text-left text-gray-800"
+                    >
+                      Total
+                    </td>
+  
+                    <td class="py-4 font-bold text-gray-800">
+                      <div
+                        class="text-sm font-bold leading-loose text-center text-primaryFont font-roboto"
+                      >
+                        ${totalProductCount}
+                      </div>
+                    </td>
+                    <td class="py-4 pl-8 font-bold text-gray-800">
+                      <div
+                        class="text-lg font-bold leading-loose text-primaryFont font-roboto"
+                      >
+                        $${totalProductPrice}
+                      </div>
+                    </td>
+                  </tr>
+      `;
+  });
+
+  modalCartBody.innerHTML = checkoutproductsHtml;
+  modalCartTotal.innerHTML = checkoutTotalHtml;
+}
